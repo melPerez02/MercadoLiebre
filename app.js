1ce73ff1336ca60c8a001fb79be1464a15a0d4c8
melPerez02 <melperez3.14@gmail.com>
2023-02-13 22:57:31 -0500

    se agrega login.html

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, './views/register.html'));
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
